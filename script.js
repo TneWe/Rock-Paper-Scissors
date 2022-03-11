66f7e1445de8f179b53f79b550527f1c076028a6
TneWe <tnevesca@gmail.com>
2022-03-11 14:50:24 +0100

    Dodan js kod i funcionalnosti

--- script.js
+++ script.js
@@ -1,15 +1,29 @@
-const selectionButtons = document.querySelector('[data-selection]')
+const selectionButtons = document.querySelectorAll('[data-selection]')
+const finalColumn = document.querySelector('[data-final-column]')
+const computerScoreSpan = document.querySelector('[data-computer-score]')
+const yourScoreSpan = document.querySelector('[data-your-score]')
+const SELECTIONS = [
+  {
+    name: 'rock',
+    beats: 'scissors'
+  },
+  {
+    name: 'paper',
+    beats: 'rock'
+  },
+  {
+    name: 'scissors',
+    beats: 'paper'
+  }
+]
 
 selectionButtons.forEach(selectionButton => {
-    selectionButton.addEventListener('click', e=>
-    {
-      const selectionName =   selectionButton.dataset.selection
-      makeSElection(selectionName)
-    }
-    )
-});
+  selectionButton.addEventListener('click', e => {
+    const selectionName = selectionButton.dataset.selection
+    const selection = SELECTIONS.find(selection => selection.name === selectionName)
+    makeSelection(selection)
+  })
+})
 
-function makeSElection(selection)
-{
-    console.log(selection)
-}
+function makeSelection(selection) {
+ console.log(selection)}
